073ed3d478d8941d6e9a3704a15d7461ad454da9
Ivan Shalaginov <shalaginov.dev@gmail.com>
2024-04-23 20:20:32 +0300

    fixed error from stop app before start

--- bot.service.ts
+++ bot.service.ts
@@ -13,21 +13,25 @@ interface MessageSending {
 export const messageSending: MessageSending = {
     url_taskMap: {},
     startMorningSending(userId) {
-        let num = 1
-        const morningTask = cron.schedule('* * * * *', () => {
-            const randomNumber = Math.floor(Math.random() * 350)
-            if (num % 2 === 0) {
-                bot.api.sendPhoto(userId, images[randomNumber])
-                num++
-            } else {
-                bot.api.sendPhoto(userId, images2[randomNumber])
-                num++
-            }
+        if (this.url_taskMap.morningJob) return
+        else {
+            let num = 1
+            const morningTask = cron.schedule('* * * * *', () => {
+                const randomNumber = Math.floor(Math.random() * 350)
+                if (num % 2 === 0) {
+                    bot.api.sendPhoto(userId, images[randomNumber])
+                    num++
+                } else {
+                    bot.api.sendPhoto(userId, images2[randomNumber])
+                    num++
+                }
 
-        })
-        this.url_taskMap['morningJob'] = morningTask
+            })
+            this.url_taskMap['morningJob'] = morningTask
+        }
     },
     stopMorningSending() {
-        this.url_taskMap.morningJob.stop()
+        if (!this.url_taskMap.morningJob) return
+        else this.url_taskMap.morningJob.stop()
     }
 }
